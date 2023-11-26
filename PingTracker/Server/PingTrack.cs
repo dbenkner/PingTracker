9f49b91876ed2089cfab651776113cb0bc22f57e
David Benkner <d.benkner@gmail.com>
2023-11-26 17:24:51 -0500

    Correct issue wsith exception thrown when there is DNS failure

--- PingTracker/Server/PingTrack.cs
+++ PingTracker/Server/PingTrack.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PingTracker.Models;
+using System.Net;
 using System.Net.NetworkInformation;
 
 
@@ -7,18 +8,28 @@ namespace PingTracker.Server
 {
     public class PingTrack
     {
+
         public static async Task<PingResult> MakePing(string url)
         {
             Ping pinger = new Ping();
-            PingReply rep = await pinger.SendPingAsync(url);
-            PingResult res = new PingResult
+            try
             {
-                Address = rep.Address.ToString(),
-                Status = rep.Status.ToString(),
-                RTT = rep.RoundtripTime,
-                DateTime = DateTime.Now,
-            };
-            return res;
+                PingReply rep = await pinger.SendPingAsync(url);
+                PingResult res = new PingResult
+                {
+                    Address = rep.Address.ToString(),
+                    Status = rep.Status.ToString(),
+                    RTT = rep.RoundtripTime,
+                    DateTime = DateTime.Now,
+                };
+                return res;
+            }
+            catch (Exception PingException)
+            {
+                Console.WriteLine(PingException.ToString(), PingException.Message);
+                return new PingResult { Status = "HOST COULD NOT BE FOUND"
+                };
+            }
         }
     }
 }
